4a5f09949957e18d2749fc9417e66c27789657ab
qazi <qaidzaidkhan@gmail.com>
2026-05-14 15:03:53 +0500

    bug fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,14 +29,6 @@ app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
-// make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 const startServer = async () => {
   try {
